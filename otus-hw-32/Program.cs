fa0384e25e13ec772f14acc26a40fd114493ffbe
o.rovenskiy@dcoa.ru <o.rovenskiy@dcoa.ru>
2023-03-14 20:27:22 +0300

    all tasks

--- otus-hw-32/Program.cs
+++ otus-hw-32/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.IO;
+using System.Text;
+using System.Xml.Linq;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+// 1.  Создать директории c:\Otus\TestDir1 и c:\Otus\TestDir2 с помощью класса DirectoryInfo
+
+
+var dir1 = new DirectoryInfo("c:\\Otus\\TestDir1");
+var dir2 = new DirectoryInfo("c:\\Otus\\TestDir2");
+
+dir1.Create();
+dir2.Create();
+
+
+// 2. В каждой директории создать несколько файлов File1...File10 с помощью класса File.
+
+
+
+
+    for (int i = 1; i < 11; i++)
+    {
+        File.Create("c:\\Otus\\TestDir1\\file" + i).Close();
+        File.Create("c:\\Otus\\TestDir2\\file" + i).Close();
+    }
+
+
+
+// 3. В каждый файл записать его имя в кодировке UTF8. Учесть, что файл может быть удален, либо отсутствовать права на запись.
+// Учесть, что файл может быть удален -- File.WriteAllText если файл удалён, то метод его создаст
+// Учесть, что могут отсутствовать права на запись -- File.Create предоставляет права на чтение и запись
+
+
+for (int i = 1; i < 11; i++)
+
+{
+
+    File.WriteAllText($"c:\\Otus\\TestDir1\\file{i}", $"file{i}", Encoding.UTF8);
+
+    //4. Каждый файл дополнить текущей датой (значение DateTime.Now) любыми способами: синхронно и\или асинхронно.
+
+    File.AppendAllText($"c:\\Otus\\TestDir1\\file{i}", ("   Date Time  " + DateTime.Now.ToString()));
+    File.AppendAllText($"c:\\Otus\\TestDir2\\file{i}", ("   Date Time  " + DateTime.Now.ToString()));
+
+}
+
+//5. Прочитать все файлы и вывести на консоль: имя_файла: текст + дополнение.
+
+for (int i = 1; i < 11; i++)
+
+{
+
+    Console.WriteLine(File.ReadAllText("c:\\Otus\\TestDir1\\file" + i));
+}
+
+Console.ReadKey();
